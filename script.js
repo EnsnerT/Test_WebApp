1e41ae695614c36fa46166ee29efd5a1fc2f1f0f
ThE <42667961+EnsnerT@users.noreply.github.com>
2020-02-20 12:48:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Site Loaded");
+
+setTimeout(function(){
+  document.body.querySelector("p").innerText = "This is an "+(navigator.onLine ? 'Online' : 'Offline')+ " Version of the Site.";
+});
