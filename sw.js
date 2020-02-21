bf525d66f0ff5849e92c2f0141931e9e8ef3eaae
ThE <42667961+EnsnerT@users.noreply.github.com>
2020-02-21 13:37:00 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -32,7 +32,7 @@ self.addEventListener('fetch', function (event) {
         );
     } else {
         event.respondWith(
-            caches.match((event.request.endsWith("/")?"/Test_WebApp/offline.html":event.request)
+            caches.match((event.request.endsWith("/")?"/Test_WebApp/offline.html":event.request))
                 .then(function (response) {
                     // Cache hit - return response
                     if (response) {
